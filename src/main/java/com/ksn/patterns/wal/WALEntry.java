875148efb5a485f2d3186639749fc72cf37a06fa
Kuldeep Singh Nauhwar <kuku@192.168.1.7>
2026-07-07 14:54:13 +0530

    feat: add wal entry

--- src/main/java/com/ksn/patterns/wal/WALEntry.java
+++ src/main/java/com/ksn/patterns/wal/WALEntry.java
@@ -1,4 +1,13 @@
 package com.ksn.patterns.wal;
 
 public class WALEntry {
+    private final Command command;
+
+    public WALEntry(Command command){
+        this.command = command;
+    }
+
+    public byte[] data(){
+        return command.serialize();
+    }
 }
